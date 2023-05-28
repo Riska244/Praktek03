dc63eaf8618c63c4992cda6e0c3b3ad67cb10dbe
riska <riskafawn@gmail.com>
2023-05-28 12:58:20 +0700

    buat method cetakinfo()

--- src/main/java/praktek03/Sepeda.java
+++ src/main/java/praktek03/Sepeda.java
@@ -12,4 +12,10 @@ public class Sepeda {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo() {
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga);
+    }
 }
